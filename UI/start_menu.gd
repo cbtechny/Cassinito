b4892eaef867445b86b0ffe0200fa0c408683868
C <chris.bido.tech@outlook.com>
2026-01-08 01:17:41 -0500

    Add card management and UI enhancements; implement card textures and animations for gameplay

--- UI/start_menu.gd
+++ UI/start_menu.gd
@@ -4,9 +4,13 @@ signal game_begin
 
 @onready var play_btn : Button = $margins/button_container/play_button
 @onready var how_to_play_btn : Button = $margins/button_container/how_to_play_button
-
+@onready var am : AnimationPlayer = $AnimationPlayer
+@onready var bg_cont : Control = $bg_container
 
 func _on_play_button_pressed() -> void:
+	am.play( "start_game_1" )
+	await am.animation_finished
+	await get_tree().create_timer( 0.2 ).timeout
 	SceneManager.go_to_scene_id( 3 )
 	emit_signal( "game_begin" )
 
