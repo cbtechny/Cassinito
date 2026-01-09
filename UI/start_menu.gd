8d9c15156eb2de28498179cf85f250f7e4e443fd
C <chris.bido.tech@outlook.com>
2026-01-08 23:54:43 -0500

    Refactor start menu and card scene; update animations, adjust layout, and enhance UI elements

--- UI/start_menu.gd
+++ UI/start_menu.gd
@@ -7,9 +7,11 @@ signal game_begin
 @onready var am : AnimationPlayer = $AnimationPlayer
 @onready var bg_cont : Control = $bg_container
 
-func _on_play_button_pressed() -> void:
-	am.play( "start_game_1" )
+func _ready() -> void:
+	am.play("intro_drop")
 	await am.animation_finished
+
+func _on_play_button_pressed() -> void:
 	await get_tree().create_timer( 0.2 ).timeout
 	SceneManager.go_to_scene_id( 3 )
 	emit_signal( "game_begin" )
